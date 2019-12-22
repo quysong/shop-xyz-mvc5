59302bf3f745db6faeddfa68e0e95ea59a710534
lyhunganh <hunganhly2208@gmail.com>
2019-12-22 13:58:52 +0700

    add project

---
+++ shop-xyz/App_Start/Startup.Auth.cs
@@ -0,0 +1,7 @@
+﻿namespace shop_xyz
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+    }
+}
